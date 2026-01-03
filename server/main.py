8904160abfed2f2c9988af93b9e08401bc16f7ee
Dag Wästberg <dwastberg@gmail.com>
2026-01-03 17:46:06 +0100

    fix cors in dev

--- server/main.py
+++ server/main.py
@@ -13,7 +13,7 @@ app = fastapi.FastAPI(title="DTCC Datsets Downloader", version="0.1.0")
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
