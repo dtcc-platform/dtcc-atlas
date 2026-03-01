3b3bc0110e665379fa2ab25a25cc92969da21528
Vasilis Naserentin <vasilis.naserentin@chalmers.se>
2026-03-01 20:33:48 +0200

    feat: expose X-Session-Id in CORS headers

--- server/main.py
+++ server/main.py
@@ -63,7 +63,7 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-    expose_headers=["Content-Disposition"],
+    expose_headers=["Content-Disposition", "X-Session-Id"],
 )
 
 app.add_middleware(
